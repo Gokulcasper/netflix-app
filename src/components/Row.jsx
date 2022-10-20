1d8160b105aeb6da2dac9c20140664ad15d65b5c
Gokulcasper <ronitcasper215@gmail.com>
2022-10-20 21:31:07 +0530

    Feat : Change Correction

---
+++ src/components/Row.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { FaHeart,FaRegHeart} from 'react-icons/fa'
+import Movie from './Movie';
+
+
+
+const Row = ({ title, fetchURL}) => {
+    const [movies, setMovies] = useState([]);
+    const [like, setLike] = useState(false)
+
+    useEffect(() => {
+      axios.get(fetchURL).then((response) => {
+        setMovies(response.data.results);
+      });
+    }, [fetchURL]);
+
+    console.log(movies);
+
+  return (
+    <>
+        <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+        <div className='relative flex items-center group'>
+        <MdChevronLeft
+          onClick={slideLeft}
+          className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+        <div
+          id={'slider' + rowID}
+          className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
+        >
+          {movies.map((item, id) => (
+            <Movie key={id} item={item} />
+          ))}
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Row
