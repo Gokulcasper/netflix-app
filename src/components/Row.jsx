da619a66430d4cf6964c380957a01c921429839f
Gokulcasper <ronitcasper215@gmail.com>
2022-10-21 22:14:28 +0530

    Feat : fixed Error

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,26 +1,30 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { FaHeart,FaRegHeart} from 'react-icons/fa'
 import Movie from './Movie';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
+const Row = ({ title, fetchURL, rowID }) => {
+  const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
 
-const Row = ({ title, fetchURL}) => {
-    const [movies, setMovies] = useState([]);
-    const [like, setLike] = useState(false)
-
-    useEffect(() => {
-      axios.get(fetchURL).then((response) => {
-        setMovies(response.data.results);
-      });
-    }, [fetchURL]);
-
-    console.log(movies);
+  const slideLeft = () => {
+    var slider = document.getElementById('slider' + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById('slider' + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
   return (
     <>
-        <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-        <div className='relative flex items-center group'>
+      <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+      <div className='relative flex items-center group'>
         <MdChevronLeft
           onClick={slideLeft}
           className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
@@ -34,9 +38,14 @@ const Row = ({ title, fetchURL}) => {
             <Movie key={id} item={item} />
           ))}
         </div>
-        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Row
+export default Row;
