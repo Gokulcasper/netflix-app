aca6085de5b2715515b70ca28c9b7d50c756d467
Gokulcasper <ronitcasper215@gmail.com>
2022-11-01 20:31:17 +0530

    Feat : changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -37,7 +37,7 @@ const Main = () => {
           <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
           <div className='my-4'>
             <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
-              PLAY
+              Play
             </button>
             <button className='border text-white border-gray-300 py-2 px-5 ml-4'>
               Watch Later
